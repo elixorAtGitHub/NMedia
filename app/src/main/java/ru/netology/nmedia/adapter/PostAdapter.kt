1b310a9d33d725e8017816d9f0066f1b605f29c0
Aleksandr Shishkin <elixor@mail.ru>
2023-04-10 19:54:21 +0300

    Realized CRUD

--- app/src/main/java/ru/netology/nmedia/adapter/PostAdapter.kt
+++ app/src/main/java/ru/netology/nmedia/adapter/PostAdapter.kt
@@ -6,23 +6,27 @@ import androidx.recyclerview.widget.ListAdapter
 import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.dto.Post
 
-typealias OnLikeClickListener = (Post) -> Unit
-typealias OnShareClickListener = (Post) -> Unit
+//typealias OnLikeClickListener = (Post) -> Unit
+//typealias OnShareClickListener = (Post) -> Unit
+//typealias OnRemoveClickListener = (Post) -> Unit
+
+interface PostListener {
+    fun onRemove(post: Post)
+    fun onEdit(post: Post)
+    fun onShare(post: Post)
+    fun onLike(post: Post)
+}
 
 class PostAdapter(
-    private val onLikeClicked: OnLikeClickListener,
-    private val onShareClicked: OnShareClickListener,
+    private val listener: PostListener,
 ) : ListAdapter<Post, PostViewHolder>(PostDiffCallback()) {
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
-        val binding =
-            CardPostBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false,
-            )
-        return PostViewHolder(binding, onLikeClicked, onShareClicked)
+        val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent,false)
+        return PostViewHolder(
+            binding = binding,
+            listener = listener
+        )
     }
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,16 +1,44 @@
 package ru.netology.nmedia.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemory
 
+private val empty = Post(
+    id = 0,
+    author = "",
+    content = "",
+    published = "",
+    likedByMe = false
+)
 class PostViewModel: ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemory()
 
     val data: LiveData<List<Post>> = repository.getAll()
     fun likeById(id:Long) = repository.likeById(id)
     fun share(id: Long) = repository.shareById(id)
+    fun remove(id: Long) = repository.removeById(id)
 
+    val edited = MutableLiveData(empty)
+
+    fun save() {
+        edited.value?.let {
+            repository.save(it)
+        }
+        edited.value = empty
+    }
+
+    fun edit(post: Post) {
+        edited.value = post
+    }
+    fun changeContent(content: String) {
+        edited.value?.let { post ->
+            if (content != post.content) {
+                edited.value = post.copy(content = content)
+            }
+        }
+    }
 }
